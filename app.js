5ce0fe990399eb6c91dec1c08d212c1ef30096ab
Brandon Johnson <bmj1985@gmail.com>
2019-06-27 17:23:34 -0600

    project setup

--- app.js
+++ app.js
@@ -8,29 +8,39 @@ const app = express()
 app.use(cors())
 app.use(morgan('dev'))
 
+const db = require('./models')
+const Category = db.Category
+
 // Router files
 
-app.get('/api/test', (req, res, next) => {
-    res.json({ message: 'Route working' })
+app.get('/api/categories', (req, res, next) => {
+  Category.findAll().then(categories =>
+    res
+      .json({
+        categories,
+      })
+      .catch(error => next(error))
+  )
 })
 
 app.use(notFound)
 app.use(errorHandler)
 
 function notFound(req, res, next) {
-    res.status(404).send({
-        error: 'Not found!',
-        status: 404,
-        url: req.originalUrl,
-    })
+  res.status(404).send({
+    error: 'Not found!',
+    status: 404,
+    url: req.originalUrl,
+  })
 }
 
 function errorHandler(err, req, res, next) {
-    console.error('ERROR', err)
-    const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
-    res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+  console.error('ERROR', err)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
 }
 
-app.listen(PORT)
-    .on('error', console.error.bind(console))
-    .on('listening', console.log.bind(console, `Listening on port: ${PORT}`))
+app
+  .listen(PORT)
+  .on('error', console.error.bind(console))
+  .on('listening', console.log.bind(console, `Listening on port: ${PORT}`))
---
+++ seeders/20190627223043-categories.js
@@ -0,0 +1,42 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert(
+      'Categories',
+      [
+        {
+          name: 'Shirts',
+        },
+        {
+          name: 'Hats',
+        },
+        {
+          name: 'Hoodies',
+        },
+      ],
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Categories', null, {})
+  },
+}
